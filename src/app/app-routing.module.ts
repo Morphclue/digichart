29dbb226e2ee275ad98a173b306de7eff5df7d50
Julian Holfeld <julian@uni-kassel.de>
2021-09-15 18:40:21 +0200

    feat: add components and editor-module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'editor', loadChildren: () => import('./editor/editor.module').then(p => p.EditorModule)},
+  {path: '', pathMatch: 'full', redirectTo: 'editor'},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/editor/editor.module.ts
@@ -0,0 +1,18 @@
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {EditorComponent} from './editor/editor.component';
+import {TreeComponent} from './tree/tree.component';
+import {EditorRoutingModule} from "./editor-routing.module";
+
+@NgModule({
+  declarations: [
+    EditorComponent,
+    TreeComponent,
+  ],
+  imports: [
+    CommonModule,
+    EditorRoutingModule,
+  ]
+})
+export class EditorModule {
+}
