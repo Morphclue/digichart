20f6d54a41c821e0a9145a6f0fa8d419f74c6ddb
Julian Holfeld <julian@uni-kassel.de>
2023-06-13 18:49:50 +0200

    feat: add basic network

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,10 @@ import {BrowserModule} from '@angular/platform-browser';
 
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
-import { NetworkComponent } from './network/network.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NetworkComponent,
   ],
   imports: [
     BrowserModule,
--- src/app/editor/tree/tree.component.ts
+++ src/app/editor/tree/tree.component.ts
@@ -1,16 +1,32 @@
-import {Component, OnInit} from '@angular/core';
+import {AfterViewInit, Component, ElementRef, ViewChild} from '@angular/core';
+import {Network} from 'vis-network';
 
 @Component({
   selector: 'app-tree',
   templateUrl: './tree.component.html',
   styleUrls: ['./tree.component.scss'],
 })
-export class TreeComponent implements OnInit {
+export class TreeComponent implements AfterViewInit {
+  @ViewChild('network') el: ElementRef | undefined;
+  private network: Network | undefined;
 
   constructor() {
   }
 
-  ngOnInit(): void {
+  ngAfterViewInit(): void {
+    const container = this.el?.nativeElement;
+    const data = {
+      nodes: [
+        {id: 1, label: 'Node 1'},
+        {id: 2, label: 'Node 2'},
+        {id: 3, label: 'Node 3'},
+      ],
+      edges: [
+        {from: 1, to: 2},
+        {from: 1, to: 3},
+      ],
+    };
+    const options = {};
+    this.network = new Network(container, data, options);
   }
-
 }
