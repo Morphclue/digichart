ac3ae3ff0b189d22305c6170ca26c6927be8c2cd
Julian Holfeld <julian@uni-kassel.de>
2023-06-13 22:00:17 +0200

    feat: css, dummydata and hierarchical layout

--- src/app/editor/network/network.component.ts
+++ src/app/editor/network/network.component.ts
@@ -17,15 +17,34 @@ export class NetworkComponent implements AfterViewInit {
     const container = this.el?.nativeElement;
     const data = {
       nodes: [
-        {id: 1, label: 'Node 1'},
-        {id: 2, label: 'Node 2'},
-        {id: 3, label: 'Node 3'},
+        {id: 1, label: 'Botamon'},
+        {id: 2, label: 'Koromon'},
+        {id: 3, label: 'Agumon'},
+        {id: 4, label: 'Greymon'},
+        {id: 5, label: 'MetalGreymon'},
+        {id: 6, label: 'WarGreymon'},
+        {id: 7, label: 'SkullGreymon'},
+        {id: 8, label: 'Tyrannomon'},
+        {id: 9, label: 'MasterTyrannomon'},
       ],
       edges: [
         {from: 1, to: 2},
-        {from: 1, to: 3},
+        {from: 2, to: 3},
+        {from: 3, to: 4},
+        {from: 3, to: 8},
+        {from: 4, to: 5},
+        {from: 5, to: 6},
+        {from: 4, to: 7},
+        {from: 8, to: 9},
       ],
     };
-    this.network = new Network(container, data, {});
+    const options = {
+      layout: {
+        hierarchical: {
+          direction: 'LR',
+        },
+      },
+    };
+    this.network = new Network(container, data, options);
   }
 }
