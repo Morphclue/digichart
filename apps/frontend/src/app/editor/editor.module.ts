d6242c2fdb07d4e7b3a753c9b138af7932a43bff
Julian Holfeld <julian@uni-kassel.de>
2023-07-15 13:15:47 +0200

    feat: navbar component & core-module

--- apps/frontend/src/app/editor/editor.module.ts
+++ apps/frontend/src/app/editor/editor.module.ts
@@ -1,10 +1,11 @@
 import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
 import {HttpClientModule} from '@angular/common/http';
 
-import {CommonModule} from '@angular/common';
+import {CoreModule} from '../core/core.module';
 import {EditorComponent} from './editor/editor.component';
-import {NetworkComponent} from './network/network.component';
 import {EditorRoutingModule} from './editor-routing.module';
+import {NetworkComponent} from './network/network.component';
 
 @NgModule({
   declarations: [
@@ -15,6 +16,7 @@ import {EditorRoutingModule} from './editor-routing.module';
     CommonModule,
     EditorRoutingModule,
     HttpClientModule,
+    CoreModule,
   ],
 })
 export class EditorModule {
